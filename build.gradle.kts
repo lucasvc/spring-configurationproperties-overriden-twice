f6b593ace17e7ae7b871d2477468e85cef0a3621
Lucas Ventura <lucas.ventura@tomtom.com>
2019-05-28 16:23:36 +0200

    - Use variable for spring-boot version - Tested using both versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,9 +7,11 @@ repositories {
     jcenter()
 }
 
+val springBootVersion = "1.5.20.RELEASE"
+                        // "2.1.5.RELEASE"
 dependencies {
-    implementation("org.springframework.boot", "spring-boot", "1.5.20.RELEASE")
+    implementation("org.springframework.boot", "spring-boot", springBootVersion)
     
-    testImplementation("junit:junit:4.12")
-    testImplementation("org.springframework.boot", "spring-boot-starter-test", "1.5.20.RELEASE")
+    testImplementation("junit", "junit", "4.12")
+    testImplementation("org.springframework.boot", "spring-boot-starter-test", springBootVersion)
 }
