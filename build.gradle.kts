8de0949b74072314c8435297db4b450c36b65709
Lucas Ventura <lucas.ventura@tomtom.com>
2019-05-28 16:22:57 +0200

    - Deleted unnecessary dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,5 +12,4 @@ dependencies {
     
     testImplementation("junit:junit:4.12")
     testImplementation("org.springframework.boot", "spring-boot-starter-test", "1.5.20.RELEASE")
-    testImplementation("org.springframework", "spring-test", "4.3.23.RELEASE")
 }
